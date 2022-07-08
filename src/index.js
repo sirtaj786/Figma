6a793753fa20f21e98256f88f40821021d54b74a
Srk78699 <ksirtaj29@gmail.com>
2022-07-08 09:54:46 +0530

    theme

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ThemeProvider } from './Context/ThemeContext';
+import { ChakraProvider } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <ChakraProvider>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
 
